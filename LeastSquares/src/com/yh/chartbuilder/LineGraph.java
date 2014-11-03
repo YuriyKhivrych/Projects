b578e1fd7904db9d7d6cd3d1c0676f2d4fce6e7e
YuriyKhivrych <yhequalizer@gmail.com>
2014-11-03 15:58:47 +0200

    LineGraph added

---
+++ LeastSquares/src/com/yh/chartbuilder/LineGraph.java
@@ -0,0 +1,63 @@
+package com.yh.chartbuilder;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.TimeSeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.widget.LinearLayout;
+
+
+
+public class LineGraph {
+	
+	private GraphicalView view;
+	
+	private TimeSeries dataset = new TimeSeries("Line Graph");
+	private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+	
+	//renderer to customize line 1
+	private XYSeriesRenderer renderer = new XYSeriesRenderer();
+	private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+	
+	public LineGraph(){
+		//Add single dataset to multiple dataset
+		mDataset.addSeries(dataset);
+		
+		
+		//Customize line 1
+		renderer.setColor(Color.WHITE);
+		renderer.setPointStyle(PointStyle.CIRCLE);
+		renderer.setFillPoints(true);	
+		
+		
+		mRenderer.setXTitle("x");
+		mRenderer.setYTitle("y");
+		mRenderer.setZoomButtonsVisible(true);
+		
+		
+		//Add single renderer to multiple renderer
+		mRenderer.addSeriesRenderer(renderer);
+		
+		
+	}
+	
+	public GraphicalView getView (Context context){
+		view = ChartFactory.getLineChartView(context, mDataset, mRenderer);
+		return view;
+		
+	}
+	
+	public void addNewPoint(Point point){
+		dataset.add(point.getX(), point.getY());
+		
+	}
+}
--- LeastSquares/src/com/yh/chartbuilder/MainXYChart.java
+++ LeastSquares/src/com/yh/chartbuilder/MainXYChart.java
@@ -1,17 +1,44 @@
 package com.yh.chartbuilder;
 
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainXYChart extends Activity implements OnClickListener {
 
+	Button btnAdd;
+	Button btnChange;
+	
+	private static GraphicalView mChart;
+	private LineGraph lineGraph = new LineGraph();
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		btnAdd = (Button) findViewById(R.id.btnAdd);
+		btnChange = (Button) findViewById(R.id.btnChange);
+		
+		btnAdd.setOnClickListener(this);
+		btnChange.setOnClickListener(this);
+		
+		
+		// Getting a reference to LinearLayout of the MainActivity Layout
+	  	LinearLayout chartContainer = (LinearLayout) findViewById(R.id.chart_container);
+	  	
+	  	mChart = lineGraph.getView(this);
+	  	
+	 	// Adding the Line Chart to the LinearLayout
+	  	chartContainer.addView(mChart);
 	}
 
 	@Override
@@ -24,7 +51,14 @@ public class MainXYChart extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+		switch(v.getId()){
+			case(R.id.btnAdd):
+				
+				
+				break;
+			
+				
+		}
 		
 		
 		
