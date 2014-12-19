74346636752c2caf9de065bec6a9543c3a1d8461
YuriyKhivrych <yhequalizer@gmail.com>
2014-12-19 12:23:56 +0200

    sync test

--- ChartBuilder/app/src/main/java/com/yh/chartbuilder/MainXYChart.java
+++ ChartBuilder/app/src/main/java/com/yh/chartbuilder/MainXYChart.java
@@ -32,7 +32,7 @@ public class MainXYChart extends Activity implements OnSharedPreferenceChangeLis
 	
 	SharedPreferences sharedPrefs;
 
-    //test
+    //sync test
 	//fields to get coordinates of points
 	EditText xValue;
 	EditText yValue;
